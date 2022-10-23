79f1afbf265a46a2170f375354fd795e6845de20
Grace Mills <gracemills.1023@gmail.com>
2022-10-22 21:24:49 -0500

    django-registration-redux and requirements update

--- blogposts/views.py
+++ blogposts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'blogposts/template.html')
+    count = Post.objects.all().count()
+    context = {
+        'count': count,
+    }
+    return render(request, 'blogposts/template.html', context)
 
 def post(request):
     return render(request, 'blogposts/post.html')
