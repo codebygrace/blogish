6038ca06ae9f2406f3fa96603ee193acc14bb470
Grace Mills <gracemills.1023@gmail.com>
2022-10-24 19:38:05 -0500

    function to create new post; automatically get user as author

--- blogposts/models.py
+++ blogposts/models.py
@@ -5,6 +5,6 @@ import datetime
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     text = models.TextField()
     publish_date = models.DateField(default=datetime.date.today)
--- blogposts/views.py
+++ blogposts/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
 
 # Create your views here.
 def index(request):
-    count = Post.objects.all().count()
+    posts = Post.objects.all()
+    count = posts.count()
     context = {
         'count': count,
+        'posts': posts,
     }
     return render(request, 'blogposts/template.html', context)
 
+def post(request, id):
+    post = Post.objects.get(id=id)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blogposts/post.html', context)
 
-def post(request):
-    return render(request, 'blogposts/post.html')
+def save_post(request):
+    title = request.POST.get('title')
+    author = request.user
+    text = request.POST.get('text')
+    new_post = Post(title=title, author=author, text=text)
+    new_post.save()
+
+def new_post(request):
+    if request.method == 'POST':
+        save_post(request)
+        return redirect('/')
+    context = {
+        'method': request.method
+    }
+    return render(request, 'blogposts/new_post.html', context)
