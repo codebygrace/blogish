5def79a071a1fb33b410e6d4b133e5b1d1aabd93
Grace Mills <gracemills.1023@gmail.com>
2023-02-06 19:56:58 -0500

    added media queries and adjusted class names on several elements

--- blog/settings.py
+++ blog/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "blogposts/static"),
+    os.path.join(BASE_DIR, "blogposts/static/"),
 ]
 
 # Default primary key field type
